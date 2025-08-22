37428bd60910932bf0a9de019f74ed4b174bd2a9
taqiismail10 <taqiismail10@gmail.com>
2025-08-23 03:04:22 +0600

    Update caching through railway

--- backend/DB/redis.config.js
+++ backend/DB/redis.config.js
@@ -1,10 +1,13 @@
 import redis from "express-redis-cache";
 
-const redisCache = redis({
-  port: 6379,
-  host: "localhost",
-  prefix: "uniform",
-  expire: 60 * 60, // 1 hour
-});
+const redisCache = redis(
+  process.env.REDIS_URL || "redis://localhost:6379", // One-liner!,
+  {
+    // host: process.env.REDISHOST || "localhost",
+    // port: process.env.REDISPORT || 6379,
+    prefix: "uniform",
+    expire: 60 * 60, // 1 hour
+  }
+);
 
 export default redisCache;
